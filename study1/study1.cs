62318b468200b0bc7a5bbb99dbbf4ab2d49034b3
cristianandraaade <cristianbrenoandrade@gmail.com>
2024-03-08 02:25:58 -0300

    baskara

---
+++ study1/study1.cs
@@ -0,0 +1,9 @@
+﻿//Declaração de variavel
+double=a,b,c,x1,x2;
+//Entrada de Dados
+Console.WriteLine("Escreva o valor de a,b,c");
+a=double.Parse(Console.ReadLine());
+b=double.Parse(Console.ReadLine());
+c=double.Parse(Console.ReadLine());
+//Processamento de dados
+x1= -b+Math.Sqrt(Math.Pow(b,2)-4*a*c)/2*a;
